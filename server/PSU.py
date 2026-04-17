d4ee64129dc127e1e9aed1577b8696d92b8dc0fb
LarsPLoge <larsloge9@gmail.com>
2026-04-17 12:32:52 +0200

    setup

--- server/PSU.py
+++ server/PSU.py
@@ -29,8 +29,8 @@ class PSU:
                 return float(voltage_str)
             if command == "MEAS:CURR?":
                 current_response = self.resource.query(command)
-                # current_str = current_response.split(",")[1].strip() for lab testing
-                current_str = current_response.split(",")[0].strip()
+                current_str = current_response.split(",")[1].strip() #for lab testing
+                #current_str = current_response.split(",")[0].strip()
                 return float(current_str)
         if self.name == "hmp4040":
             response = self.resource.query(command)
