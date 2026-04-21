79a64c4e066bd4db99f543bfa6ad6fd78fbafab8
LarsPLoge <larsloge9@gmail.com>
2026-04-21 10:21:03 +0200

    fix

--- server/Helper.py
+++ server/Helper.py
@@ -61,16 +61,16 @@ class Helper:
                 return cli_command
         raise ValueError(f"Unknown scpi command: {scpi_cmd}")
     
-    def make_queue(psu, server):
-        queue = None
-        if psu.name == "k6500":
-            queue = k6500Queue(psu=psu, server=server)
-        elif psu.name == "hmp4040":
-            queue = hmp4040Queue(psu=psu, server=server)
-        elif psu.name == "k2400":
-            queue = k2400Queue(psu=psu, server=server)
-        elif psu.name == "k2450":
-            queue = k2450Queue(psu=psu, server=server)
-        else:
-            raise ValueError(f"Unknown PSU name: {psu.name}")
-        return queue
+def make_queue(psu, server):
+    queue = None
+    if psu.name == "k6500":
+        queue = k6500Queue(psu=psu, server=server)
+    elif psu.name == "hmp4040":
+        queue = hmp4040Queue(psu=psu, server=server)
+    elif psu.name == "k2400":
+        queue = k2400Queue(psu=psu, server=server)
+    elif psu.name == "k2450":
+        queue = k2450Queue(psu=psu, server=server)
+    else:
+        raise ValueError(f"Unknown PSU name: {psu.name}")
+    return queue
--- server/server.py
+++ server/server.py
@@ -1,7 +1,7 @@
 import pyvisa
 import zmq
 
-from .Helper import Helper
+from .Helper import make_queue
 from .psu_queue import PSUQueue
 from .PSU import PSU
 from logger import setup_logger
@@ -109,7 +109,7 @@ class Server:
         psu.address = address
         psu.connected = True
         self.psus[psu_name] = psu
-        self.psu_queues[psu_name] = Helper.make_queue(psu=psu, server=self)
+        self.psu_queues[psu_name] = make_queue(psu=psu, server=self)
 
         logger.info(f'connected psu: {psu.name}')
 
